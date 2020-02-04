a69801311795d31863a73f75e9cd7fcc05225f89
PALLAVI CHOUHAN <pallavichouhan04@gmail.com>
2020-02-05 01:04:39 +0200

    committing pod files and project settings

---
+++ DVTWeather/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  DVTWeather
+//
+//  Created by Pallavi Chouhan on 2020/01/29.
+//  Copyright © 2020 Pallavi Chouhan. All rights reserved.
+//
+
+struct K {
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "ForecastTableViewCell"
+    
+    static let favSegueIdentifier = "favoriteView"
+    static let weatherMapViewIdentifier = "weatherMapView"
+    
+    static let favouriteAlertMsg =  "The current location is saved as favourite"
+    
+    
+    static let lastUpdatedTime = "TheLastTimeUserOpenedTheApp"
+    static let favouriteUserLocation  = "FavouriteUserLocation"
+}
